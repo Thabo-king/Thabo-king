3ea331498b9a92dade299bae9ed3ced61ee63f1e
Thabo-king <97471520+Thabo-king@users.noreply.github.com>
2024-11-01 02:37:49 +0000

    Update loadScript.js

--- src/src/loadScript.js
+++ src/src/loadScript.js
@@ -1,1 +1,16 @@
+export function loadSlaterScript() {
+  const version = "1.0";
+  const isStaging = window.location.host.includes("webflow.io");
+  const scriptSrc = isStaging
+    ? "https://slater.app/5072/10063.js"
+    : `https://assets.slater.app/slater/5072/10063.js?v=${version}`;
+
+  const script = document.createElement("script");
+  script.src = scriptSrc;
+  script.type = "text/javascript";
+  document.body.appendChild(script);
+
+  script.onload = () => console.log("Slater script loaded successfully.");
+  script.onerror = (error) => console.error("Error loading Slater script", error);
+}
 

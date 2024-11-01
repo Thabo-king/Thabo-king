7d41cbd5e71f77e4d4689c57d939b08825a96be1
Thabo-king <97471520+Thabo-king@users.noreply.github.com>
2024-11-01 02:39:34 +0000

    Update main.js

--- src/src/src/main.js
+++ src/src/src/main.js
@@ -1,1 +1,7 @@
+import { loadSlaterScript } from './loadScript.js';
+import { initializeAnimations } from './animations.js';
 
+document.addEventListener("DOMContentLoaded", function () {
+  loadSlaterScript();
+  initializeAnimations();
+});
